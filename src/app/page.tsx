d57549e1dde8142972276a379af0e44293c9758f
ummesalma303 <ummesalma333999@gmail.com>
2025-04-08 16:52:35 +0600

    Added upload button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,39 @@
-// import Image from "next/image";
-import Button from '@mui/material/Button';
-export default function Home() {
-  return (
-    <div>
-      <Button variant="contained">Hello world</Button>
+"use client"
+import React, { useState } from 'react'
 
+// import Button from '@mui/material/Button';
+
+
+const Home:React.FC = () => {
+  const [files, setFile] = useState<File | null>(null);
+  if (!files) {
+    return
+  }else{
+    const formData = new FormData()
+    formData.append("File",files)
+    formData.append("upload_preset", "test_one");
+    console.log(formData)
+
+  }
+  return (
+    <div className='w-full h-screen bg-gray-100 flex justify-center items-center'>
+      <div className=' border-2 border-dashed h-44 w-74'>
+        
+                <label htmlFor="image" className="inline-block px-6 py-3 bg-green-400 text-white mb-2 text-sm">
+                  Select Image:
+                <input onChange={(e)=>setFile(e.target.files ? e.target.files[0]:null )} className='hidden'
+                  required
+                  type="file"
+                  id="image"
+                  name="image"
+                  accept="image/*"
+                />
+                </label>
+              </div>
+
+       {/* <Button variant="contained">Upload</Button> */}
     </div>
-  );
+  )
 }
+
+export default Home
