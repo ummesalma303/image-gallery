d325c799e197c357f985e92ea52bd5be9a0c1c46
ummesalma303 <ummesalma333999@gmail.com>
2025-04-09 13:37:51 +0600

    complete image uploading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,46 @@
 "use client"
+import Image from 'next/image'
 import React, { useState } from 'react'
 
 // import Button from '@mui/material/Button';
 
 
 const Home:React.FC = () => {
-  // const [files, setFile] = useState<File | null>(null);
+  const [image, setImage] = useState< string>('');
 
-  const handleImage = async (imgUrl: React.ChangeEvent<HTMLInputElement>)=>{
-    // console.log(e)
-    if (!imgUrl) {
-      // console.log(imgUrl)
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>)=>{
+    const selectedFile = e.target.files?.[0]
+   
+  if (!selectedFile) {
+    console.log("No file selected");
       return
     }else{
-      // console.log(imgUrl)
-      const formData = new FormData()
-      formData.append("file",imgUrl)
-      formData.append("upload_preset", "test_one");
-      console.log(formData)
-
-     
-      
+      try {
+        
+        const formData = new FormData()
+        formData.append("file", selectedFile)
+        formData.append("upload_preset", "image_gallery");
+        const data = await fetch('https://api.cloudinary.com/v1_1/dfieeuqta/image/upload',{
+          method: 'POST',
+          body: formData
+        })
+        const res = await data.json()
+        setImage(res.url)
+      console.log(res.url)
+      } catch (error) {
+        console.log(error)
+      }
      
     }
   }
   
   return (
     <div className='w-full h-screen bg-gray-100 flex justify-center items-center'>
-      <div className=' border-2 border-dashed h-44 w-74'>
+      <div className=''>
         
                 <label htmlFor="image" className="inline-block px-6 py-3 bg-green-400 text-white mb-2 text-sm">
                   Select Image:
-                <input onChange={(e)=>handleImage(e.target.files?.[0] || null )} className='hidden'
+                <input onChange={(e)=>handleFileChange(e)} className='hidden'
                   required
                   type="file"
                   id="image"
@@ -40,7 +49,12 @@ const Home:React.FC = () => {
                 />
                 </label>
               </div>
+              <div className="">
+               {image && <Image src={image} width={90} height={40} alt='uploaded'/>}
+               
+  
 
+              </div>
        {/* <Button variant="contained">Upload</Button> */}
     </div>
   )
