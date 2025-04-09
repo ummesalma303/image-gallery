bea32f08d78c8e332ba1b9ce6c0eb44ef84fb59d
ummesalma303 <ummesalma333999@gmail.com>
2025-04-09 12:22:23 +0600

    Added cloudinary base url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,23 +5,33 @@ import React, { useState } from 'react'
 
 
 const Home:React.FC = () => {
-  const [files, setFile] = useState<File | null>(null);
-  if (!files) {
-    return
-  }else{
-    const formData = new FormData()
-    formData.append("File",files)
-    formData.append("upload_preset", "test_one");
-    console.log(formData)
+  // const [files, setFile] = useState<File | null>(null);
 
+  const handleImage = async (imgUrl: React.ChangeEvent<HTMLInputElement>)=>{
+    // console.log(e)
+    if (!imgUrl) {
+      // console.log(imgUrl)
+      return
+    }else{
+      // console.log(imgUrl)
+      const formData = new FormData()
+      formData.append("file",imgUrl)
+      formData.append("upload_preset", "test_one");
+      console.log(formData)
+
+     
+      
+     
+    }
   }
+  
   return (
     <div className='w-full h-screen bg-gray-100 flex justify-center items-center'>
       <div className=' border-2 border-dashed h-44 w-74'>
         
                 <label htmlFor="image" className="inline-block px-6 py-3 bg-green-400 text-white mb-2 text-sm">
                   Select Image:
-                <input onChange={(e)=>setFile(e.target.files ? e.target.files[0]:null )} className='hidden'
+                <input onChange={(e)=>handleImage(e.target.files?.[0] || null )} className='hidden'
                   required
                   type="file"
                   id="image"
